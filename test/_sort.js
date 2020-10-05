6bf8c63ff31a89a837d8d6e9f62e7db7eef2cee8
Nina Matti <ninadesireematti@gmail.com>
2020-10-05 15:50:20 +0900

    added test for mergeSort that checks if Array was sorted properly

--- test/_sort.js
+++ test/_sort.js
@@ -5,7 +5,12 @@ describe("Sort", () => {
   it("should be a function", () => {
     expect(Sort).to.be.a("function");
   });
-  it("should have a sort method", () => {
+  it("should have a mergeSort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
+  });
+  it("should sort an array in ascending order", () => {
+    let testArray = new Sort([5, 8, 1, 3, 9, 2, 6, 4, 7]);
+    let sortedArray = testArray.mergeSort();
+    expect(sortedArray).to.equal([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 });
