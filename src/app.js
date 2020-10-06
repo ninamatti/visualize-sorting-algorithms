162f37252a292cc1547aa099779800dc4b3388b3
Nina Matti <ninadesireematti@gmail.com>
2020-10-07 00:04:25 +0900

    added settimeout functions to make partial arrays appear in order

--- src/app.js
+++ src/app.js
@@ -3,6 +3,55 @@ require("./index.css");
 
 console.log("we made it into the app!!");
 
+// get the display elements!!
+
+// level 1
+let levelOne = document.getElementById("level1");
+
+let levelOneArrayOne = document.getElementById("level1array1");
+let levelOneArrayTwo = document.getElementById("level1array2");
+
+// level 2
+let levelTwo = document.getElementById("level2");
+
+let levelTwoArrayOne = document.getElementById("level2array1");
+let levelTwoArrayTwo = document.getElementById("level2array2");
+let levelTwoArrayThree = document.getElementById("level2array3");
+let levelTwoArrayFour = document.getElementById("level2array4");
+
+// level 3
+let levelThree = document.getElementById("level3");
+
+let levelThreeArrayOne = document.getElementById("level3array1");
+let levelThreeArrayTwo = document.getElementById("level3array2");
+let levelThreeArrayThree = document.getElementById("level3array3");
+let levelThreeArrayFour = document.getElementById("level3array4");
+let levelThreeArrayFive = document.getElementById("level3array5");
+let levelThreeArraySix = document.getElementById("level3array6");
+let levelThreeArraySeven = document.getElementById("level3array7");
+let levelThreeArrayEight = document.getElementById("level3array8");
+
+// level 4
+let levelFour = document.getElementById("level4");
+
+let levelFourArrayOne = document.getElementById("level4array1");
+let levelFourArrayTwo = document.getElementById("level4array2");
+let levelFourArrayThree = document.getElementById("level4array3");
+let levelFourArrayFour = document.getElementById("level4array4");
+
+// level 5
+let levelFive = document.getElementById("level5");
+
+let levelFiveArrayOne = document.getElementById("level5array1");
+let levelFiveArrayTwo = document.getElementById("level5array2");
+
+// level 6
+let levelSix = document.getElementById("level6");
+
+let levelSixArrayOne = document.getElementById("level6array1");
+
+console.log(levelOneArrayOne);
+
 let inputArray = [];
 
 let button = document.getElementById("submitButton");
@@ -22,7 +71,93 @@ button.addEventListener("click", () => {
   let mergeFunction = new Sort(inputArray);
   let resultArray = mergeFunction.divideAndConquer(inputArray);
 
+  console.log("resultArray: ", resultArray);
+
+  // fill in display elements
+
+  // level 1
+  levelOneArrayOne.innerText = inputArray.slice(0, 4);
+  levelOneArrayTwo.innerText = inputArray.slice(4);
+
+  // level 2
+  levelTwoArrayOne.innerText = inputArray.slice(0, 2);
+  levelTwoArrayTwo.innerText = inputArray.slice(2, 4);
+  levelTwoArrayThree.innerText = inputArray.slice(4, 6);
+  levelTwoArrayFour.innerText = inputArray.slice(6);
+
+  // level 3
+  levelThreeArrayOne.innerText = inputArray[0];
+  levelThreeArrayTwo.innerText = inputArray[1];
+  levelThreeArrayThree.innerText = inputArray[2];
+  levelThreeArrayFour.innerText = inputArray[3];
+  levelThreeArrayFive.innerText = inputArray[4];
+  levelThreeArraySix.innerText = inputArray[5];
+  levelThreeArraySeven.innerText = inputArray[6];
+  levelThreeArrayEight.innerText = inputArray[7];
+
+  // level 4
+  levelFourArrayOne.innerText = resultArray.slice(0, 2);
+  levelFourArrayTwo.innerText = resultArray.slice(2, 4);
+  levelFourArrayThree.innerText = resultArray.slice(4, 6);
+  levelFourArrayFour.innerText = resultArray.slice(6);
+
+  // level 5
+  levelFiveArrayOne.innerText = resultArray.slice(0, 4);
+  levelFiveArrayTwo.innerText = resultArray.slice(4);
+
+  // level 6
+  levelSixArrayOne.innerText = resultArray;
+
   console.log("output: ", resultArray);
+  console.log("children of level one: ", levelOne.children);
+
+  // make level 1 appear by removing hidden and adding fade-in class
+  setTimeout(() => {
+    for (let child of levelOne.children) {
+      child.classList.remove("hidden");
+      child.classList.add("fade-in");
+    }
+  }, 2000);
+
+  // make level 2 appear by removing hidden and adding fade-in class
+  setTimeout(() => {
+    for (let child of levelTwo.children) {
+      child.classList.remove("hidden");
+      child.classList.add("fade-in");
+    }
+  }, 4000);
+
+  // make level 3 appear by removing hidden and adding fade-in class
+  setTimeout(() => {
+    for (let child of levelThree.children) {
+      child.classList.remove("hidden");
+      child.classList.add("fade-in");
+    }
+  }, 6000);
+
+  // make level 4 appear by removing hidden and adding fade-in class
+  setTimeout(() => {
+    for (let child of levelFour.children) {
+      child.classList.remove("hidden");
+      child.classList.add("fade-in");
+    }
+  }, 8000);
+
+  // make level 5 appear by removing hidden and adding fade-in class
+  setTimeout(() => {
+    for (let child of levelFive.children) {
+      child.classList.remove("hidden");
+      child.classList.add("fade-in");
+    }
+  }, 10000);
+
+  // make level 6 appear by removing hidden and adding fade-in class
+  setTimeout(() => {
+    for (let child of levelSix.children) {
+      child.classList.remove("hidden");
+      child.classList.add("fade-in");
+    }
+  }, 12000);
 });
 
 /* Example Code
